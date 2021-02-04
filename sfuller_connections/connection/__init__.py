ce67a89a46b007cddfe9614ac5ccd8f509904e62
captify-sfuller <52924007+captify-sfuller@users.noreply.github.com>
2021-02-04 14:29:05 +0000

    Delete sfuller_connections/connection directory

--- sfuller_connections/connection/__init__.py
+++
@@ -1,4 +0,0 @@
-from .configs import ImpalaConfig, ImpalaConfigFromEnv, S3Config, S3ConfigFromEnv
-
-from .impala_connection import ImpalaConnect
-from .s3_connection import S3Connect, S3ConnectCSV
--- sfuller_connections/connection/configs/__init__.py
+++
@@ -1,5 +0,0 @@
-from .impala import ImpalaConfig
-from .impala import ImpalaConfigFromEnv
-from .s3 import S3Config
-from .s3 import S3ConfigFromEnv
-
