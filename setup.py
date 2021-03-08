9a56efc36fa6a265660db319602c8e014410fe32
captify-sfuller <52924007+captify-sfuller@users.noreply.github.com>
2021-03-08 17:04:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sfuller_connections',
-      version='0.5',
+      version='0.6',
       description='connections shortcut',
       packages=['sfuller_connections'],
       author_email='sebastian.fuller@captify.co.uk',
