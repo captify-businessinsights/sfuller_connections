9dff991dc5913542368074f3455cc7f59d8cb6ca
captify-sfuller <sebastian.fuller@captify.co.uk>
2021-06-30 15:00:16 +0100

    better error reporting

--- sfuller_connections/impala_connection.py
+++ sfuller_connections/impala_connection.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
+from logging import error
 import pandas as pd
 from impala.dbapi import connect
 from .impala import ImpalaConfig
 import os
+import time
+from datetime import datetime
 
+def time_since(start_time):
+    current_time = time.time()
+    time_taken = current_time - start_time
+    if time_taken > 60:
+        time_taken = round(time_taken/60, 1)
+        units = "minutes"
+    else:
+        time_taken = round(time_taken, 0)
+        units = "seconds"
+    
+    return current_time, f"{time_taken} {units}"
 
 class ImpalaConnect:
     def __init__(self, query, config: ImpalaConfig):
@@ -11,11 +25,14 @@ class ImpalaConnect:
         self.config = config
 
     def get_impala_df(self, request_pool=os.getenv("REQUEST_POOL"), mem_limit="40g"):
+        start_time = time.time()
+        
         with connect(host=self.config.host,
                      port=self.config.port,
                      user=self.config.user,
                      password=self.config.password,
-                     timeout=self.config.timeout,
+                    #  timeout=self.config.timeout,
+                     timeout = 1,
                      use_ssl=True,
                      auth_mechanism=self.config.auth_mechanism) as conn:
 
@@ -24,7 +41,22 @@ class ImpalaConnect:
 
                 i = 1
                 for q in queries:
-                    cur.execute(q, configuration={"REQUEST_POOL": request_pool, "MEM_LIMIT": mem_limit})
+                    try:
+                        cur.execute(q, configuration={"REQUEST_POOL": request_pool, "MEM_LIMIT": mem_limit})
+                    except Exception as e:
+                        error_time, length_str = time_since(start_time)
+
+                        print("\n")
+                        print(type(e).__name__)
+                        if e.args[0]:
+                            print(e.args[0])
+                        print(f"Start time: {datetime.utcfromtimestamp(int(start_time)).strftime('%Y-%m-%d %H:%M:%S')}")
+                        print(f"Error hit at: {datetime.utcfromtimestamp(int(error_time)).strftime('%Y-%m-%d %H:%M:%S')} ({length_str})\n")
+                        print("Offending query:\n")
+                        print(q)
+                        print("\n")
+                        raise e
+
                     if len(queries) > 1:
                         print(f'executing query {i} of {len(queries)}')
                     try:
