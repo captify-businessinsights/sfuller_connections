eb2d3cc32d9b4e665d1d38153a712be348352e52
captify-sfuller <sebastian.fuller@captify.co.uk>
2021-12-02 09:44:46 +0000

    Update impala.py

--- sfuller_connections/impala.py
+++ sfuller_connections/impala.py
@@ -10,11 +10,15 @@ class ImpalaConfig(NamedTuple):
     timeout: int
     auth_mechanism: str
 
-
-ImpalaConfigFromEnv = ImpalaConfig(host=os.getenv("IMPALA_HOST"),
-                                   port=int(os.getenv("IMPALA_PORT")),
-                                   user=os.getenv("IMPALA_USER"),
-                                   password=os.getenv("IMPALA_PASSWORD"),
-                                   timeout=int(os.getenv("IMPALA_TIMEOUT")),
-                                   auth_mechanism=os.getenv("IMPALA_AUTH_MECHANISM"))
-
+try:
+    ImpalaConfigFromEnv = ImpalaConfig(host=os.getenv("IMPALA_HOST"),
+                                       port=int(os.getenv("IMPALA_PORT")),
+                                       user=os.getenv("IMPALA_USER"),
+                                       password=os.getenv("IMPALA_PASSWORD"),
+                                       timeout=int(os.getenv("IMPALA_TIMEOUT")),
+                                       auth_mechanism=os.getenv("IMPALA_AUTH_MECHANISM"))
+except Error as e:
+    print("problem setting up environmental variables")
+    print("please refer to the environmental variable setup doc")
+    print("https://captify.atlassian.net/wiki/spaces/PT/pages/520847628/Environmental+Variables")
+    raise e
